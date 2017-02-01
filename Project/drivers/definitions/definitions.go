a11517e3554c663d6beac844ec4e244b4903c662
eirikjohannes <larsen.eirikj@gmail.com>
2017-02-01 14:12:27 +0100

    Added definitions files, added hardware files

---
+++ Project/drivers/definitions/definitions.go
@@ -0,0 +1,12 @@
+/*Definition file for elevator project
+Group 13, Einar Henriksen, Eirik Larsen
+*/
+
+package definitions
+
+import "time"
+
+const NumFloors = 4
+const NumButtons = 3
+const ElevatorTimeoutDuration = 5 * time.Second
+const AliveMessageInterval = 500 * time.Millisecond

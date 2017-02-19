007f45c0bb36ca19c90ab6678b7bad945ce2e538
einarama <henriksen.einar@gmail.com>
2017-02-19 17:30:24 +0100

    started queue module

---
+++ Project/src/queue/cost.go
@@ -0,0 +1,38 @@
+package queue
+
+import def "definitions"
+
+func CalculateCost(currentDir, currentFloor, prevFloor, targetFloor, targetButton int) int{
+	totalCost := 0
+	dir       := currentDir
+	targetDir := targetFloor-prevFloor
+
+	if currentFloor == -1 {
+		totalCost++
+	} else if dir != def.DirIdle {
+		totalCost += 2
+	}
+	if dir != def.DirIdle {
+		if targetDir != dir {
+			totCost += 10
+		}
+	}
+
+	if targetDir > 0 && dir == def.DirUp || dir == def.DirIdle {
+		for floor := prevFloor; floor < targetFloor || floor == def.NumFloors; floor++ {
+			if queue.hasLocalRequest(floor, targetButton) || queue.hasLocalRequest(floor, def.BtnInside) {
+				totalCost++
+			}
+			totalCost++
+		}
+	}
+	if targetDir < 0 && dir == def.DirDown || dir == def.DirIdle {
+		for floor := prevFloor; floor > targetFloor || floor == 0; floor-- {
+			if queue.hasLocalRequest(floor, targetButton) || queue.hasLocalRequest(floor, def.BtnInside) {
+				totalCost++
+			}
+			totalCost++
+		}
+	}
+	return totalCost
+}

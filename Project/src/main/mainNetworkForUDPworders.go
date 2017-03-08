3dc9daea879e7ab2a22541683279eeb0e270f2f3
Student <student@itk-d650>
2017-03-08 13:07:10 +0100

    Ready for testing

--- Project/src/main/mainNetworkForUDPworders.go
+++ Project/src/main/mainNetworkForUDPworders.go
@@ -6,8 +6,10 @@ import (
 	"fmt"
 	"fsm"
 	"hardware"
+	"log"
 	"network"
 	"os"
+	"os/signal"
 	"queue"
 	"time"
 )
@@ -25,51 +27,24 @@ func main() {
 	}
 	hardwareCh := def.HardwareChan{
 		MotorDir:       make(chan int),
-		FloorLamp:      make(chan int),
+		FloorLamp:      make(chan int, 2),
 		DoorLamp:       make(chan bool),
 		BtnPressed:     make(chan def.ButtonPress, 10),
 		DoorTimerReset: make(chan bool),
 	}
 	currentFloor := hardware.Init()
 	time.Sleep(time.Millisecond * 500)
-	go network.InitUDP(messageCh.Incoming, messageCh.Outgoing, eventCh.ElevatorPeerUpdate)
-	go EventHandler(eventCh, messageCh, hardwareCh)
 	go fsm.Init(eventCh, hardwareCh, currentFloor)
+	go network.InitUDP(messageCh.Incoming, messageCh.Outgoing, eventCh.ElevatorPeerUpdate)
+	go queue.RunBackup(messageCh.Outgoing)
+	go EventHandler(eventCh, messageCh, hardwareCh)
 	time.Sleep(time.Millisecond * 500)
-	go assigner.CollectCost(messageCh.CostReply)
-	go queue.RunBackup()
-	go PrintOrder(messageCh.Incoming, messageCh.Outgoing)
-	order := def.Message{Category: def.NewOrder, Floor: 1, Button: 2, Cost: 0, Addr: def.LocalElevatorId}
+	go assigner.CollectCosts(messageCh.CostReply)
 
-	orderDistributeTimer := time.NewTicker(time.Second * 2)
 	go safeKill()
-	for {
-		<-orderDistributeTimer.C
-		messageCh.Outgoing <- order
-		messageCh.Outgoing <- def.Message{def.CompleteOrder, 1, 2, 0, def.LocalElevatorId}
-	}
-
-}
-
-func PrintOrder(incomingMsg chan def.Message, outgoingMsg chan def.Message) {
-	for {
-		orderToPrint := <-incomingMsg
-		switch orderToPrint.Category {
-		case def.Alive:
-			fmt.Println("Alive message recieved")
-		case def.NewOrder:
-			fmt.Println("NEW order recieved! Fantastic news\n")
-			fmt.Println("The new order has ID %s", orderToPrint.Addr)
-			fmt.Println("Floor; %i \t Button; %i \t", orderToPrint.Floor, orderToPrint.Button)
-		case def.CompleteOrder:
-			fmt.Println("\n\tCOMPLETED order!\n")
-			fmt.Println("The completed order has ID %s", orderToPrint.Addr)
-			fmt.Println("Floor; %i \t Button; %i \t", orderToPrint.Floor, orderToPrint.Button)
-		case def.Cost:
-			fmt.Println("Cost msg recieved")
-		}
-
-	}
+	fmt.Println("Started main")
+	hold := make(chan bool)
+	<-hold
 
 }
 
@@ -77,6 +52,6 @@ func safeKill() {
 	var c = make(chan os.Signal)
 	signal.Notify(c, os.Interrupt)
 	<-c
-	hardware.SetMotorDir(def.DirStop)
+	hardware.SetMotorDir(def.DirIdle)
 	log.Fatal(def.Col0, "User terminated program.", def.ColN)
 }
